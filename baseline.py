6e5bae13b6f2bad23ddacfe87fab237f9b74af80
William <rileyneyland7@gmail.com>
2025-04-22 23:05:14 -0500

    Adds baseline directory traversal

--- baseline.py
+++ baseline.py
@@ -1,4 +1,5 @@
 import pathlib
+import os
 
 '''Class for creating directory FIM baselines'''
 
@@ -8,13 +9,32 @@ class baseline():
         self.dir = dir
 
         # Type check the dir attribute
-        assert type(self.dir) == pathlib.PosixPath, "Type error with self.dir"
+        assert type(self.dir) == pathlib.PosixPath, "Type error with self.dir" # The value of self.dir passed should be of type pathlib.PosixPath
+        #self.dir = os.fspath(self.dir) # Cast type of self.dir from pathlib.PosixPath to str
 
-        self.test()
+        self.dirWalk()
 
-    def test(self):
+    def dirWalk(self):
         print("You're in the test() method of the baseline class")
-        print(self.dir)
+        cache = open('./cache/testCache.txt', 'r+', encoding='utf-8')
+
+        # pathlib.walk() returns a three value tuple (root, dirs, files) where root = type pathlib.Posixpath | dirs = list of strings | files = list of strings
+
+        # Prints out dir being walked prints and nested loops are used for testing
+        for root, dirs, files in self.dir.walk():
+            print(f'root of type {type(root)} = {root}\ndir of type {type(dirs)} = {dirs}\nfile of type {type(files)} = {files}\n')
+            if len(dirs) > 0:
+                i = 0
+                for dir in dirs:
+                    print(f'dir at position {i} of type {type(dir)} = {dir}')
+                    i += 1
+            if len(files) > 0:
+                i = 0
+                for file in files:
+                    print(f'file at position {i} of type {type(file)} = {file}')
+                    i += 1
+
+
 
 #    os.stat() could be used for file attributes
 #    shutil.copystat() could be used for writing stat attributes to a baseline file
