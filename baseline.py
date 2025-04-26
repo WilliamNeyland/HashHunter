cab6cd471a004e284470a4cb082e4a46753983d4
William <rileyneyland7@gmail.com>
2025-04-26 17:51:41 -0500

    Writes file attributes to cache. Removed unecessary code

--- baseline.py
+++ baseline.py
@@ -5,7 +5,6 @@ import os
 
 class baseline():
     def __init__(self, dir):
-        print("You are in the baseline __init__ method")
         self.dir = dir
 
         # Type check the dir attribute
@@ -15,30 +14,14 @@ class baseline():
         self.dirWalk()
 
     def dirWalk(self):
-        print("You're in the test() method of the baseline class")
         cache = open('./cache/testCache.txt', 'r+', encoding='utf-8')
 
         # pathlib.walk() returns a three value tuple (root, dirs, files) where root = type pathlib.Posixpath | dirs = list of strings | files = list of strings
-
-        # Prints out dir being walked prints and nested loops are used for testing
         for root, dirs, files in self.dir.walk():
             cache.write(str(root) + '\n')
             cache.write(str(dirs) + '\n')
             cache.write(str(files) + '\n')
-            print(f'root of type {type(root)} = {root}\ndir of type {type(dirs)} = {dirs}\nfile of type {type(files)} = {files}\n')
-            if len(dirs) > 0:
-                i = 0
-                for dir in dirs:
-                    print(f'dir at position {i} of type {type(dir)} = {dir}')
-                    i += 1
-            if len(files) > 0:
-                i = 0
-                for file in files:
-                    print(f'file at position {i} of type {type(file)} = {file}')
-                    i += 1
+            for file in files:
+                cache.write(f'attributes of {root / file}: ' + str(os.stat(root / file)))
 
-
-
-#    os.stat() could be used for file attributes
-#    shutil.copystat() could be used for writing stat attributes to a baseline file
-#    Path can be used for dir paths
+        cache.close()
