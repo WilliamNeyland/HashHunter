41a86d4568aa90f17c55bddff8ba43cce7fbfb8e
William <rileyneyland7@gmail.com>
2025-04-28 00:30:14 -0500

    Creates existing baseline cache & checks if specified baseline already exists

--- main.py
+++ main.py
@@ -1,17 +1,56 @@
 from pathlib import Path
 from baseline import *
+import os
 
 def createBaseline():
     print("Please enter the path of the directory you would like to baseline:")
     print("[example: '/home/william/Documents']")
     path = Path(input("> "))
+
     if path.exists():
         print(f"{path} is valid")
-        #print(type(path))
-        baseline(path)
     else:
         print("Invalid path please try again.")
         createBaseline()
+
+    baselineCache = Path(str(os.getcwd())+'/cache/baselineCache.txt')
+    if baselineCache.exists():
+        # Investigate if existingBaselines.close() statements are needed
+        with open(baselineCache, 'r') as existingBaselines:
+            for lines in existingBaselines:
+                if lines.strip() == str(path):
+                    print("WARNING: A Baseline for the directory provided already exists.\n"
+                    "Choose:\n"
+                    "1 -- Return to Main Menu\n"
+                    "2 -- Overwrite Existing Baseline (THERE IS NO GOING BACK)")
+                    tempInput = int(input("> "))
+                    if tempInput == 1:
+                        existingBaselines.close()
+                        main()
+                    elif tempInput == 2:
+                        existingBaselines.close()
+                        baseline(path)
+                        break
+                    else:
+                        print("Invalid selection please try again.")
+                        existingBaselines.close()
+                        createBaseline()
+                else:
+                    with open(baselineCache, 'a') as existingBaselines:
+                        existingBaselines.write(str(path)+'\n')
+                        existingBaselines.close()
+                        baseline(path)
+                        break
+    else:
+        temp = os.getcwd()
+        os.chdir(str(os.getcwd()+'/cache'))
+        with open("baselineCache.txt", "w", encoding="utf-8") as baselineCacheFile:
+            baselineCacheFile.write(str(path)+'\n')
+        os.chdir(str(temp))
+        baseline(path)
+                
+
+    
 
 def main():
     print("Welcome to HashHunter an open source signature based FIM and AV tool!")
