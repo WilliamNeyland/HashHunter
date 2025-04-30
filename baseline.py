8a0bd42b66eb62705e8ef0001c566bc22202df73
William <rileyneyland7@gmail.com>
2025-04-30 00:24:29 -0500

    Creates unique baseline file & obfuscates baseline file name

--- baseline.py
+++ baseline.py
@@ -1,23 +1,39 @@
 import pathlib
 import os
-import stat
+import hashlib
 from fileStruct import *
+from pathlib import *
 
 '''Class for creating directory FIM baselines'''
 
 class baseline():
     def __init__(self, dir):
-        self.dir = dir
+        self.baselineDir = dir
+        self.cacheFileName = ''
+        self.cacheFileDir = ''
         # Type check the dir attribute
-        assert type(self.dir) == pathlib.PosixPath, "Type error with self.dir" # The value of self.dir passed should be of type pathlib.PosixPath
-        #self.dir = os.fspath(self.dir) # Cast type of self.dir from pathlib.PosixPath to str
+        assert type(self.baselineDir) == pathlib.PosixPath, "Type error with self.baselineDir"
 
+        self.makeBaselineFile()
         self.dirWalk()
 
+    def makeBaselineFile(self):
+        # This will create a baseline file to keep the specified paths baseline in a unique file
+        # This will need to be stored in an obfuscated manner
+        # File name will be obfuscated
+        # A dictionary will be created with the path as the key and obfuscated baseline filename as the entry -- PROBABLY NOT NEEDED
+        # This dictionary will need to be stored and loaded into memory when the program runs
+
+        temp = str(self.baselineDir)
+        self.cacheFileName = hashlib.sha256(temp.encode(encoding='utf-8', errors='strict')).hexdigest()
+        self.cacheFileDir = os.getcwd() + '/cache/baselines/' + self.cacheFileName + '.txt'
+        file = open(self.cacheFileDir,'w')
+        file.close()
+
     def dirWalk(self):
-        with open('./cache/testCache.txt', 'w', encoding='utf-8') as cache:
+        with open(self.cacheFileDir, 'w', encoding='utf-8') as cache:
             # pathlib.walk() returns a three value tuple (root, dirs, files) where root = type pathlib.Posixpath | dirs = list of strings | files = list of strings
-            for root, dirs, files in self.dir.walk():
+            for root, dirs, files in self.baselineDir.walk():
                 cache.write(str(root) + '\n')
                 cache.write(str(dirs) + '\n')
                 cache.write(str(files) + '\n')
