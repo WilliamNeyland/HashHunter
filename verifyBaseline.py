d1fed373130624f6f613e01166a747eab7fc7a03
William <rileyneyland7@gmail.com>
2025-05-11 21:45:55 -0500

    WIP - Reading/Parsing Baseline Data Structure

--- verifyBaseline.py
+++ verifyBaseline.py
@@ -1,6 +1,7 @@
 import pathlib
 import os
 import hashlib
+import json
 
 class verifyBaseline():
     
@@ -8,15 +9,43 @@ class verifyBaseline():
         self.baselinePath = baselinePath
 
         self.makeHash()
+        self.loadCacheBaseline()
+
 
     # CLEAN UP
     # PoC in Current State
     def makeHash(self):
         cacheBaselineName = hashlib.sha256(str(self.baselinePath).encode(encoding='utf-8', errors='strict')).hexdigest()
-        cacheBaselinePath = os.getcwd() + '/cache/baselines/' + cacheBaselineName + '.txt'
-        with open (cacheBaselinePath, 'r') as cacheBaseline:
+        self.cacheBaselinePath = os.getcwd() + '/cache/baselines/' + cacheBaselineName + '.txt'
+    
+    def loadCacheBaseline(self):
+        with open (self.cacheBaselinePath, 'r') as cacheBaseline:
+            self.jsonBaseline = json.load(cacheBaseline) # Loads the JSON file as a list
 
-            print(f'===== BASELINE: {self.baselinePath} =====')
+            self.cacheBaselineRoots = []
+            self.cacheBaselineDirs = []
+            self.cacheBaselineFileDicts = []
 
-            for lines in cacheBaseline:
-                print(lines)
+            for x in range(len(self.jsonBaseline)):
+                self.cacheBaselineRoots.append(self.jsonBaseline[x][0])
+                self.cacheBaselineDirs.append(self.jsonBaseline[x][1])
+                self.cacheBaselineFileDicts.append(self.jsonBaseline[x][2])
+
+            #for x in range(len(self.cacheBaselineFileDicts)):
+
+
+            
+
+    def getCurrentBaseline(self):
+        pass
+
+#[root,[dirs],{fileName:{attributeKey:value}}]
+# Verify Root Dirs
+# Verify Dirst within Root
+# Verify Files Within root
+# Verify attributes for files within root
+
+#keys = list(jsonBaseline[0][2].keys())
+#values = list(jsonBaseline[0][2].values())
+#keys2 = list(values[0].keys())
+#values2 = list(values[0].values())
